83cb6b7da8d84acf7651438d3d09fcff6b619a96
Shivamshah-06 <shivamshah93865@gmail.com>
2024-05-07 16:08:52 +0530

    ui

--- router/auth-router.js
+++ router/auth-router.js
@@ -7,14 +7,13 @@ const { signupSchema, loginSchema } = require("../validators/auth-validator");
 const authMiddleware = require("../middleware/auth-middleware");
 const { service } = require("../controller/service-controller");
 
-router.route("/").get("hello world");
 router.route("/register").post(validate(signupSchema), register);
 router.route("/login").post(validate(loginSchema), login);
 router.route("/contact").post(contact);
 router.route("/user").get(authMiddleware, user);
 router.route("/service").get(service);
-// router.get("/register", (req, res) => {
-//   res.status(201).send("register from router");
-// });
+router.get("/", (req, res) => {
+  res.status(201).send("register from router");
+});
 // router.route("/hii").get(hii);
 module.exports = router;
